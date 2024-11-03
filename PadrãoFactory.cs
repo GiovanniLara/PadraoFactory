466c833a13f056ae4f90a560b5b507bedd151737
GiovanniLara <silvagiovanni615@gmail.com>
2024-11-03 14:55:50 -0300

    Add files via upload

---
+++ PadrãoFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MetodoFactory
+{
+    public abstract class Notificacao
+    {
+        public abstract void Enviar(string mensagem);
+        public abstract Notificacao CriarNotificacao(); 
+    }
+
+    public class EmailNotificacao : Notificacao
+    {
+        public override void Enviar(string mensagem)
+        {
+            Console.WriteLine("Enviando e-mail: " + mensagem);
+        }
+
+        public override Notificacao CriarNotificacao()
+        {
+            return new EmailNotificacao();
+        }
+    }
+
+    public class SMSNotificacao : Notificacao
+    {
+        public override void Enviar(string mensagem)
+        {
+            Console.WriteLine("Enviando SMS: " + mensagem);
+        }
+
+        public override Notificacao CriarNotificacao()
+        {
+            return new SMSNotificacao();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notificacao fabrica = new EmailNotificacao();
+            Notificacao notificacao = fabrica.CriarNotificacao();
+            notificacao.Enviar("Olá! Esta é uma notificação de e-mail.");
+
+            fabrica = new SMSNotificacao();
+            notificacao = fabrica.CriarNotificacao();
+            notificacao.Enviar("Olá! Esta é uma notificação de SMS.");
+        }
+    }
+}
